65d2d09da7956f92288786783f10d1ccbf9fcf1c
Haiqelhakeem <haiqelhakeem@gmail.com>
2023-10-19 16:27:25 +0700

    (add) Input Component

---
+++ src/component/InputField.jsx
@@ -0,0 +1,9 @@
+const InputField = () => {
+    return(
+    <>
+    <input type="text" placeholder="Type here" className="input input-bordered input-info w-full max-w-xs" />
+    </>
+    )
+}
+
+export default InputField;
