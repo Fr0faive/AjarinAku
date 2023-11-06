c7a68642d3a49cc1ecbca90ab43e34a15a6665f8
fr0faive <faikar.moch.tajudin@gmail.com>
2023-11-06 11:34:27 +0700

    ga tau

--- src/Layout/NavigationBar.jsx
+++ src/Layout/NavigationBar.jsx
@@ -3,9 +3,26 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "../component/Button";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Dialog } from "@headlessui/react";
 
 const NavigationBar = () => {
+  const navigation = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "Explore",
+      href: "/explore",
+    },
+    {
+      name: "Randomize",
+      href: "/randomize",
+    },
+  ]
   const [changeColor, setChangeColor] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const changeNavbarColor = () => {
     if (window.scrollY >= 80) {
@@ -32,7 +49,7 @@ const NavigationBar = () => {
               className="h-[51px]"
             />
           </Link>
-          <div className="flex flex-row items-center">
+          <div className="hidden lg:flex flex-row items-center">
             <input
               type="text"
               placeholder="Search..."
@@ -49,16 +66,58 @@ const NavigationBar = () => {
             </a>
           </div>
         </div>
-        <div className="flex flex-wrap gap-5 items-center mx-9">
-          <Link to="#" className="text-biruTua text-lg mr-4">
-            Randomize
-          </Link>
-          <Link to="#" className="text-biruTua text-lg mr-4">
-            Explore
-          </Link>
+        <div className="hidden lg:flex lg:flex-wrap gap-5 items-center mx-9">
+          {navigation.map((item) => (
+            <Link to={item.href} className="text-biruTua text-lg mr-4">{item.name}</Link>
+          ))}
           <Button children="Login" color="primary" link="/login" />
         </div>
+        <div className="flex lg:hidden mx-9 items-center gap-5">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
       </nav>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-50" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between mx-3">
+            <a
+              href="#"
+              className="-m-1.5 p-1.5 flex justify-between items-center "
+            >
+              <img className="h-[51px] w-auto" src="/assets/ajarinaku.svg" alt="AjarinAku" />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+              {navigation.map((item) => (
+            <Link to={item.href} className="text-biruTua -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50">{item.name}</Link>
+          ))}
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
     </header>
   );
 };
