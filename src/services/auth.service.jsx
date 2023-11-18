b59806719aa83ea915be77e1ded859a9377d05a9
fr0faive <faikar.moch.tajudin@gmail.com>
2023-11-19 00:36:02 +0700

    Penambahan register service

---
+++ src/services/auth.service.jsx
@@ -0,0 +1,23 @@
+export const login = async () => {
+  fetch("https://fakestoreapi.com/auth/login", {
+    method: "POST",
+    body: JSON.stringify({
+      username: "mor_2314",
+      password: "83r5^_",
+    }),
+  })
+    .then((res) => res.json())
+    .then((json) => console.log(json));
+};
+
+export const register = async () => {
+  fetch("https://fakestoreapi.com/auth/login", {
+    method: "POST",
+    body: JSON.stringify({
+      username: "mor_2314",
+      password: "83r5^_",
+    }),
+  })
+    .then((res) => res.json())
+    .then((json) => console.log(json));
+};
