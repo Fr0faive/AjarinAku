233ca00d56ff5099f2934cdce9bb039c367e251b
fr0faive <faikar.moch.tajudin@gmail.com>
2023-11-28 20:07:59 +0700

    [Improve] Get Service

--- src/services/category.service.jsx
+++ src/services/category.service.jsx
@@ -24,4 +24,36 @@ const createCategory = async (categoryData, token) => {
   }
 };
 
-export default { createCategory };
+const getAllCategory = async (token) => {
+  try {
+    // Panggil API untuk login menggunakan Axios
+    const response = await axios.get(`${API_URL}/api/categories/all`, {
+      headers: {
+        Authorization: `${token}`, // Sertakan token otorisasi di sini
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    // Menghandle kesalahan selama proses login
+    throw error.response ? error.response.data : error.message;
+  }
+};
+
+const getCategory = async (id, token) => {
+  try {
+    // Panggil API untuk login menggunakan Axios
+    const response = await axios.get(`${API_URL}/api/categories/all/${id}`, {
+      headers: {
+        Authorization: `${token}`, // Sertakan token otorisasi di sini
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    // Menghandle kesalahan selama proses login
+    throw error.response ? error.response.data : error.message;
+  }
+};
+
+export default { createCategory, getAllCategory, getCategory };
