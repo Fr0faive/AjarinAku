2bba5ef28e1b1a44172daa5cb35f23e108565a3f
Haiqelhakeem <haiqelhakeem@gmail.com>
2023-12-12 10:41:42 +0700

    [clean] article side

--- src/Layout/ArticleSide.jsx
+++ src/Layout/ArticleSide.jsx
@@ -16,8 +16,8 @@ const ArticleSide = () => {
   return (
     <>
       <div className="flex flex-col justify-center gap-3 text-black">
-        <div className="mr-[189px] bg-white rounded-lg p-5">
-          <span className="text-2xl font-normal">Share</span>
+        <div className="mr-[189px] bg-white rounded-lg p-5 w-[245px]">
+          <span className="text-2xl font-semibold flex justify-center">Share</span>
           <div className="mt-5 flex flex-row gap-3 mx-auto">
             <FacebookShareButton url="https://ajarinaku.vercel.app">
               <div className="border-solid border-[#00419E] border-[1.8px] w-[42px] h-[42px] flex justify-center items-center rounded-sm">
@@ -57,11 +57,11 @@ const ArticleSide = () => {
             </TwitterShareButton>
           </div>
         </div>
-        <div className="mr-[189px] h-96 bg-white rounded-lg">
-          <span>Related Content</span>
+        <div className="mr-[189px] h-96 bg-white font-semibold rounded-lg w-[245px] flex justify-center">
+          <span className="mt-3">Related Content</span>
         </div>
-        <div className="mr-[189px] h-96 bg-white rounded-lg">
-          <span>Choosen Articles</span>
+        <div className="mr-[189px] h-96 bg-white font-semibold rounded-lg w-[245px] flex justify-center">
+          <span className="mt-3">Choosen Articles</span>
         </div>
       </div>
     </>
