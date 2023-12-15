b92112418101e89428428800ace19bdf947886a1
Haiqelhakeem <haiqelhakeem@gmail.com>
2023-12-15 20:25:08 +0700

    [add] category button at CategoryElement

--- src/components/Elements/Category/CategoryElement.jsx
+++ src/components/Elements/Category/CategoryElement.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import categoryService from "../../../services/category.service";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const CategoryElement = () => {
   const [dataCategory, setDataCategory] = useState([]);
@@ -11,15 +13,20 @@ const CategoryElement = () => {
   }, []);
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-[#EFF0F4] text-gray-900">
-      <h1>Category</h1>
-      <table>
+      <h1 className="text-3xl font-bold mb-4 underline">Category List</h1>
+      <button className="btn bg-blue-500 hover:bg-blue-700 text-white border-none mb-3">
+        <a href="">
+          <FontAwesomeIcon icon={faPlus} /> Add Category
+        </a>
+      </button>
+      <table className="table bg-white w-1/3 my-3">
         <thead>
-          <tr>
+          <tr className="text-black text-lg">
             <th>No</th>
             <th>Category Name</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="font-semibold">
           {dataCategory.map((data) => (
             <tr>
               <td>{data.category_id}</td>
